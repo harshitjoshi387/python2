423a25dc116638a3f89a41ced7d5d58b489e9d68
Harshit joshi <harshjoshi25022002@gmail.com>
2026-03-20 19:45:30 +0530

    chnage

---
+++ tuple3.py
@@ -0,0 +1,12 @@
+# tuple banaya
+fruits = ("apple", "banana", "mango")
+
+# print tuple
+print(fruits)
+
+# access element
+print(fruits[0])   # apple
+
+# loop ke through print
+for fruit in fruits:
+    print(fruit)
