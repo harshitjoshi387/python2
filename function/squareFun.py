7351c947c39b8d21de784737c0cfcc6e2cb31c55
Harshit joshi <harshjoshi25022002@gmail.com>
2026-03-21 19:29:09 +0530

    function

---
+++ function/squareFun.py
@@ -0,0 +1,13 @@
+# function define kar rahe hain
+def square(num):
+    result = num * num
+    return result
+
+# user se input lena
+number = int(input("Enter a number: "))
+
+# function call karna
+output = square(number)
+
+# result print karna
+print("Square is:", output)
