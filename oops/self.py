27d85ff8483a6b53b1b2259fc7fa1a183c9534cf
Harshit joshi <harshjoshi25022002@gmail.com>
2026-05-18 13:33:21 +0530

    dundar method

--- oops/self.py
+++ oops/self.py
@@ -2,11 +2,20 @@ class Employee:
     language="python"
     salary=12000
 
+    def __init__(self):  #dunder method which is automatically called
+        print('i am creating a object')
+
+
     def getInfo(self):
         print(f"the language is {self.language}")
+
+    @staticmethod #static
+    def greet():
+        print("Good morning")
 
 harry = Employee()
 harry.language= "java"
 
 print(harry.language,harry.salary)
 harry.getInfo()
+harry.greet()
